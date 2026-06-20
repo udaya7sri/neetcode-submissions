3a89c728d5c4cfc1776259ea405ca3d1f039ca8e
udaya7sri <42762327+udaya7sri@users.noreply.github.com>
2026-06-20 17:46:27 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 56.4
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Udaya"
+print(type(variable))
+variable = [1,3]
+print(type(variable))
