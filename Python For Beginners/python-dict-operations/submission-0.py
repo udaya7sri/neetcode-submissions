12a6f0b633a77e16e13db213656d1f5981d373a6
udaya7sri <42762327+udaya7sri@users.noreply.github.com>
2026-06-21 12:03:50 +0530

    Add: python-dict-operations - submission-0

---
+++ Python For Beginners/python-dict-operations/submission-0.py
@@ -0,0 +1,13 @@
+your_dict = { 
+  "a": 10, 
+  "apple": 12,
+  "bat": 7
+}
+print(your_dict)
+print(your_dict["a"])
+if("d" in your_dict):
+    print("True")
+else:
+    print("False")
+your_dict["a"]=4
+print(your_dict)
